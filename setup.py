dba7f7bbe51b545658b627cf99c2945e7758a5d4
Thiago Avelino <thiagoavelinoster@gmail.com>
2013-08-10 04:54:04 -0300

    create setup file

---
+++ setup.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+from setuptools import setup, find_packages
+
+import nacho
+
+
+REQUIREMENTS = [i.strip() for i in open("requirements.txt").readlines()]
+
+
+
+setup(name='nacho',
+    version=nacho.__version__,
+    description=nacho.__description__,
+    long_description=nacho.__description__,
+    author=nacho.__author__,
+    license=nacho.__license__,
+    packages=find_packages(exclude=('doc', 'docs', 'example')),
+    namespace_packages=['nacho'],
+    package_dir={'nacho': 'nacho'},
+    install_requires=REQUIREMENTS,
+    scripts=['nacho/bin/nacho'],
+    include_package_data=True)
