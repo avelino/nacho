7f81f42253ca0f0a04a4a5be6c1fcf8bd2eedaf7
Thiago Avelino <thiagoavelinoster@gmail.com>
2013-08-10 02:26:56 -0300

    Add nacho package

---
+++ nacho/__init__.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+from __future__ import absolute_import, division, print_function, with_statement
+
+
+VERSION = (0, 1)
+__version__ = ".".join(map(str, VERSION))
+__status__ = "Development"
+__description__ = "Pythonic web micro-framework"
+__author__ = "Thiago Avelino"
+__license__ = u"MIT License"
