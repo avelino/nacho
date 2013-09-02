4a3cd404de497efac9148e897cfb009e5a19dba8
Thiago Avelino <thiagoavelinoster@gmail.com>
2013-09-02 08:50:27 -0300

    Set install tulip

--- setup.py
+++ setup.py
@@ -5,10 +5,6 @@ from setuptools import setup, find_packages
 import nacho
 
 
-REQUIREMENTS = [i.strip() for i in open("requirements.txt").readlines()]
-
-
-
 setup(name='nacho',
     version=nacho.__version__,
     description=nacho.__description__,
@@ -17,9 +13,5 @@ setup(name='nacho',
     license=nacho.__license__,
     packages=find_packages(exclude=('doc', 'docs', 'example')),
     package_dir={'nacho': 'nacho'},
-    dependency_links=[
-        'https://tulip.googlecode.com/archive/8d0ca0be7f5ad5bf0012c2b440ca725ee395354d.zip#egg=tulip'
-    ],
-    install_requires=REQUIREMENTS,
     #scripts=['nacho/bin/nacho'],
     include_package_data=True)
