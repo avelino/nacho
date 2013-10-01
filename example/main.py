66039d5238c7c18156f6a5bbbe8c28232bb65483
Thiago Avelino <thiagoavelinoster@gmail.com>
2013-10-01 02:46:39 -0300

    Add sample used StaticFile server

--- example/main.py
+++ example/main.py
@@ -7,7 +7,7 @@ assert sys.version >= '3.3', 'Please use Python 3.3 or higher.'
 from nacho.routing import Router
 from nacho.http import HttpServer
 from nacho.multithreading import Superviser
-from nacho.app import Application
+from nacho.app import Application, StaticFile
 
 
 class Home(Application):
@@ -18,6 +18,8 @@ class Home(Application):
 
 def urls():
     router = Router()
+    router.add_handler('/static/',
+                       StaticFile('/Users/avelino/projects/nacho/example/'))
     router.add_handler('/(.*)', Home())
     return HttpServer(router, debug=True, keep_alive=75)
 
