57a98c81ca004e88fccc8af2b65455f06525f3c8
Thiago Avelino <thiagoavelinoster@gmail.com>
2013-10-01 02:44:20 -0300

    Fix quiz render

--- nacho/renderers/quik.py
+++ nacho/renderers/quik.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-from quik import CachingFileLoader
+from quik import FileLoader
 
 
 class QuikWorker(object):
@@ -8,6 +8,6 @@ class QuikWorker(object):
         self.template_dirs = template_dirs
 
     def render(self, template_name, *args, **kwargs):
-        loader = CachingFileLoader(self.template_dirs[0])
+        loader = FileLoader(self.template_dirs[0])
         template = loader.load_template(template_name)
-        return template.merge(kwargs, loader=loader).encode('utf-8')
+        return template.render(kwargs, loader=loader).encode('utf-8')
